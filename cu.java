5f1c91bb42b0848de536b496d931b0e0fa7da6d6
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-05 11:10:04 +0900

    Update cu.java

--- cu.java
+++ cu.java
@@ -1,14 +1,21 @@
 package cu;
-import java.util.Scanner;
+import java.util.Random;
 import java.util.*;
-public class a {
-	public static void main(String[] args) {
+public class Main {
+      public static void main(String args[]){
+    Television tv1 = new Television();
+
+    tv1.dispChannel();
+  }
+}
+class Television{
+	public  void dispChannel() {
 		Scanner scanner = new Scanner(System.in);
 		String old = scanner.next();
         // 呼び出し（new した後で、引数へ与えられた値を元に Pokemon が生成されます）
-        dog.Pokemon pikachu = new dog.Pokemon("こんにちは","さん","ステータス","HP：","MP：","攻撃力：","素早さ：","防御力：","さあ冒険に出かけよう！");
+　　　　dog.Pokemon pikachu = new dog.Pokemon("こんにちは","さん","ステータス","HP：","MP：","攻撃力：","素早さ：","防御力：","さあ冒険に出かけよう！");
         // 使用時
-         System.out.println(pikachu.aisatu2()+old+pikachu.san2());
+        System.out.println(pikachu.aisatu2()+old+pikachu.san2());
         System.out.println(pikachu.name2());
         System.out.println(pikachu.category2()+pikachu.num);
         System.out.println(pikachu.type2()+pikachu.num2);
@@ -18,4 +25,6 @@ public class a {
         System.out.println(pikachu.saa2());
         
     }
+    
+
 }
