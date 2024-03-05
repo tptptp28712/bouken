543ce0e0ce293b2aeab9995509e4208ec3e99043
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-05 10:51:36 +0900

    Create cu.java

---
+++ cu.java
@@ -0,0 +1,21 @@
+package cu;
+import java.util.Scanner;
+import java.util.*;
+public class a {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String old = scanner.next();
+        // 呼び出し（new した後で、引数へ与えられた値を元に Pokemon が生成されます）
+        dog.Pokemon pikachu = new dog.Pokemon("こんにちは","さん","ステータス","HP：","MP：","攻撃力：","素早さ：","防御力：","さあ冒険に出かけよう！");
+        // 使用時
+         System.out.println(pikachu.aisatu2()+old+pikachu.san2());
+        System.out.println(pikachu.name2());
+        System.out.println(pikachu.category2()+pikachu.num);
+        System.out.println(pikachu.type2()+pikachu.num2);
+        System.out.println(pikachu.description2()+pikachu.num3);
+        System.out.println(pikachu.sp2()+pikachu.num4);
+        System.out.println(pikachu.gd2()+pikachu.num5);
+        System.out.println(pikachu.saa2());
+        
+    }
+}
